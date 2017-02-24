a3f6307e56541b4c376d7fa3d031444f9238f8ba
Yuriy Yakymchuk <yyakymchuk@lohika.com>
2017-02-24 17:40:27 +0200

    Reading/Writting from/in file

--- src/main/java/com/yuriy/main/MainRun.java
+++ src/main/java/com/yuriy/main/MainRun.java
@@ -1,36 +1,68 @@
 package com.yuriy.main;
 
-import com.google.common.base.Function;
-import com.google.common.base.Joiner;
-import com.google.common.collect.Lists;
-import com.yuriy.model.Cat;
-import com.yuriy.model.Dog;
-import com.yuriy.model.Pet;
-import com.yuriy.model.PetType;
-
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+
+import com.google.common.base.Objects;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 
 /**
  * Created by yyakymchuk on 1/10/2017.
  */
 public class MainRun {
 
-    public static void main(String[] args) {
-        Pet dog = new Dog(3.2, "Barsik", PetType.DOG);
-        List<Pet> dogs = new ArrayList<Pet>();
-        dogs.add(dog);
-        dogs.add(new Dog(4.5, "Djeck", PetType.DOG));
-        List<Cat> cats = Lists.transform(dogs, new Function<Pet, Cat>() {
-            @Override
-            public Cat apply(Pet pet) {
-                Cat cat = new Cat(pet.getWeight(), pet.getName(), PetType.CAT);
-                return cat;
-            }
-        });
-        String joinedCats = Joiner.on("; ").join(dog.getName(), dog.getWeight());
-        System.out.println(String.format("Dogs: %s", dogs));
-        System.out.println(String.format("Cats: %s", cats));
-        System.out.println(String.format("Joined cats: %s", joinedCats));
+    private final static String FILE_PATH = "D:\\Trane(Emerald)\\Logs\\ProcessedLogs\\";
+    private final static String FILE_NAME = "13-14-output-50.json";
+
+    public static void main(String[] args) throws IOException {
+		printResults();
     }
+
+	private static void printResults() throws IOException{
+		final BufferedReader bufferedReader = Files.newBufferedReader(Paths.get(FILE_PATH + FILE_NAME));
+		final BufferedWriter bufferedWriter = Files.newBufferedWriter(Paths.get(FILE_PATH + "formatted_output_13-14.json"));
+		final Map<String, List<String>> objectTypeProperties = Maps.newTreeMap();
+		final List<String> objectTypes = Lists.newArrayList();
+		Stream<String> lines = bufferedReader.lines();
+		lines.forEach(s1 -> {
+
+			if (!Pattern.matches("\\s*@\\S+\\s\\d+", s1)) {
+				if (!Objects.equal("Groups", s1) && !s1.contains("timestamp")) {
+					objectTypeProperties.put(s1, Lists.newArrayList());
+					objectTypes.add(s1);
+				}
+			} else {
+				s1 = s1.substring(0, s1.lastIndexOf(":"));
+				objectTypeProperties.get(objectTypes.get(objectTypes.size() - 1)).add(s1);
+			}
+		});
+
+		objectTypeProperties.forEach((s, strings) -> {
+			try {
+				bufferedWriter.write(s + "\n");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			Collections.sort(strings);
+			strings.forEach(s1 -> {
+				try {
+					bufferedWriter.write(" "+s1+"\n");
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			});
+		});
+
+		bufferedReader.close();
+		bufferedWriter.close();
+	}
 }
